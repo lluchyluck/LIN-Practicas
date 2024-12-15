b0d90421dadd61a9274a30f7ff43aa5be925e739
Lucas Calzada <77804198+lluchyluck@users.noreply.github.com>
2024-12-15 18:52:13 +0100

    buzzer casi terminao

--- practica5/PARTEB/buzzer.c
+++ practica5/PARTEB/buzzer.c
@@ -9,71 +9,91 @@
 #include <linux/delay.h>
 #include <linux/slab.h>
 #include <linux/miscdevice.h>
+#include <linux/uaccess.h>
+#include <linux/timer.h>
 
 #define DEVICE_NAME "buzzer"
 #define PWM_DEVICE_NAME "pwmchip0"
-
-struct pwm_device *pwm_device = NULL;
-struct pwm_state pwm_state;
-static char *melody_buffer;
-static struct mutex buzzer_mutex;
-static struct work_struct my_work;
 
 struct music_step {
     int frequency;
     int duration;
 };
 
+struct pwm_device *pwm_device = NULL;
+struct pwm_state pwm_state;
+static char *melody_buffer;
+static int beat = 120; // Default beat value
+static int current_note = 0; // Current note being played
+static size_t step_count = 0;
+static struct music_step steps[100]; // Parsed melody
+static struct mutex buzzer_mutex;
+static struct work_struct my_work;
+static struct timer_list melody_timer;
+
 static inline int calculate_delay_ms(unsigned int note_len, unsigned int qnote_ref)
 {
-	unsigned char duration = (note_len & 0x7f);
-	unsigned char triplet = (note_len & 0x80);
-	unsigned char i = 0;
-	unsigned char current_duration;
-	int total = 0;
+    unsigned char duration = (note_len & 0x7f);
+    unsigned char triplet = (note_len & 0x80);
+    unsigned char i = 0;
+    unsigned char current_duration;
+    int total = 0;
 
-	/* Calculate the total duration of the note
-	 * as the summation of the figures that make
-	 * up this note (bits 0-6)
-	 */
-	while (duration) {
-		current_duration = (duration) & (1 << i);
+    while (duration) {
+        current_duration = (duration) & (1 << i);
 
-		if (current_duration) {
-			/* Scale note accordingly */
-			if (triplet)
-				current_duration = (current_duration * 3) / 2;
-			/*
-			 * 24000/qnote_ref denote number of ms associated
-			 * with a whole note (redonda)
-			 */
-			total += (240000) / (qnote_ref * current_duration);
-			/* Clear bit */
-			duration &= ~(1 << i);
-		}
-		i++;
-	}
-	return total;
+        if (current_duration) {
+            if (triplet)
+                current_duration = (current_duration * 3) / 2;
+            total += (240000) / (qnote_ref * current_duration);
+            duration &= ~(1 << i);
+        }
+        i++;
+    }
+    return total;
 }
+
 static inline unsigned int freq_to_period_ns(unsigned int frequency)
 {
-	if (frequency == 0)
-		return 0;
-	else
-		return DIV_ROUND_CLOSEST_ULL(100000000000UL, frequency);
+    if (frequency == 0)
+        return 0;
+    else
+        return DIV_ROUND_CLOSEST_ULL(100000000000UL, frequency);
 }
 
-static int buzzer_open(struct inode *inode, struct file *file) {
-    if (!mutex_trylock(&buzzer_mutex)) {
-        return -EBUSY;
+static void play_next_note(struct work_struct *work);
+
+static void timer_callback(struct timer_list *timer)
+{
+    if (current_note < step_count) {
+        schedule_work(&my_work);
+    } else {
+        pwm_disable(pwm_device);
     }
-    return 0;
 }
 
-static int buzzer_release(struct inode *inode, struct file *file) {
-    mutex_unlock(&buzzer_mutex);
-    return 0;
+static void play_next_note(struct work_struct *work)
+{
+    if (current_note >= step_count) {
+        pwm_disable(pwm_device);
+        return;
+    }
+
+    pwm_state.period = freq_to_period_ns(steps[current_note].frequency);
+    pwm_disable(pwm_device);
+
+    if (pwm_state.period > 0) {
+        pwm_set_relative_duty_cycle(&pwm_state, 70, 100);
+        pwm_state.enabled = true;
+        pwm_apply_state(pwm_device, &pwm_state);
+    } else {
+        pwm_disable(pwm_device);
+    }
+
+    mod_timer(&melody_timer, jiffies + msecs_to_jiffies(calculate_delay_ms(steps[current_note].duration, beat)));
+    current_note++;
 }
+
 static void parse_notes(const char *input_buffer, struct music_step *steps, size_t *step_count)
 {
     char *local_buffer;
@@ -87,7 +107,6 @@ static void parse_notes(const char *input_buffer, struct music_step *steps, size
         return;
     }
 
-
     while ((note = strsep(&local_buffer, ",")) != NULL) {
         if (sscanf(note, "%d:%x", &steps[index].frequency, &steps[index].duration) == 2) {
             index++;
@@ -97,100 +116,110 @@ static void parse_notes(const char *input_buffer, struct music_step *steps, size
     *step_count = index;
     kfree(local_buffer);
 }
-static void my_wq_function(struct work_struct *work)
-{
-    static struct music_step steps[100];
-    size_t step_count;
-    const int beat = 120;
 
-    parse_notes(melody_buffer, steps, &step_count);
-
-    pwm_init_state(pwm_device, &pwm_state);
-    size_t i;
-    for (i = 0; i < step_count; i++) {
-        pwm_state.period = freq_to_period_ns(steps[i].frequency);
-
-        pwm_disable(pwm_device);
-
-        if (pwm_state.period > 0) {
-            pwm_set_relative_duty_cycle(&pwm_state, 70, 100);
-            pwm_state.enabled = true;
-            pwm_apply_state(pwm_device, &pwm_state);
-        } else {
-            pwm_disable(pwm_device);
-        }
-
-        msleep(calculate_delay_ms(steps[i].duration, beat));
-        printk(KERN_INFO "Printeando nota: %d\n", steps[i].frequency);
+static int buzzer_open(struct inode *inode, struct file *file) {
+    if (!mutex_trylock(&buzzer_mutex)) {
+        return -EBUSY;
     }
-
-    pwm_disable(pwm_device);
+    return 0;
 }
 
+static int buzzer_release(struct inode *inode, struct file *file) {
+    mutex_unlock(&buzzer_mutex);
+    return 0;
+}
 
 static ssize_t buzzer_write(struct file *file, const char __user *buffer, size_t len, loff_t *offset) {
+    char *kbuf;
+
     if (len > PAGE_SIZE) {
         return -EINVAL;
     }
 
-    melody_buffer = kzalloc(len + 1, GFP_KERNEL);
-    if (!melody_buffer) {
+    kbuf = kzalloc(len + 1, GFP_KERNEL);
+    if (!kbuf) {
         return -ENOMEM;
     }
 
-    if (copy_from_user(melody_buffer, buffer, len)) {
-        kfree(melody_buffer);
+    if (copy_from_user(kbuf, buffer, len)) {
+        kfree(kbuf);
         return -EFAULT;
     }
-    printk(KERN_INFO "Se ha detectado escritura");
-    schedule_work(&my_work);
+
+    if (strncmp(kbuf, "beat", 4) == 0) {
+        int new_beat;
+        if (sscanf(kbuf + 5, "%d", &new_beat) == 1) {
+            beat = new_beat;
+            printk(KERN_INFO "buzzer: Beat updated to %d\n", beat);
+        } else {
+            printk(KERN_ERR "buzzer: Invalid beat value\n");
+            kfree(kbuf);
+            return -EINVAL;
+        }
+    } else if (strncmp(kbuf, "music", 5) == 0) {
+        melody_buffer = kzalloc(len - 5 + 1, GFP_KERNEL);
+        if (!melody_buffer) {
+            kfree(kbuf);
+            return -ENOMEM;
+        }
+        strcpy(melody_buffer, kbuf + 6);
+        printk(KERN_INFO "buzzer: Melody received\n");
+
+        parse_notes(melody_buffer, steps, &step_count);
+        current_note = 0;
+
+        schedule_work(&my_work);
+    } else {
+        printk(KERN_ERR "buzzer: Unknown command\n");
+        kfree(kbuf);
+        return -EINVAL;
+    }
+
+    kfree(kbuf);
     return len;
+}
+
+static ssize_t buzzer_read(struct file *file, char __user *buffer, size_t len, loff_t *offset) {
+    char kbuf[32];
+    int ret;
+
+    snprintf(kbuf, sizeof(kbuf), "beat=%d\n", beat);
+    ret = simple_read_from_buffer(buffer, len, offset, kbuf, strlen(kbuf));
+
+    return ret;
 }
 
 static struct file_operations fops = {
     .owner = THIS_MODULE,
     .write = buzzer_write,
+    .read = buzzer_read,
     .open = buzzer_open,
     .release = buzzer_release,
 };
+
 static struct miscdevice buzzer_misc = {
-	.minor = MISC_DYNAMIC_MINOR, /* kernel dynamically assigns a free minor# */
-	.name = DEVICE_NAME,		 /* when misc_register() is invoked, the kernel
-								  * will auto-create device file;
-								  * also populated within /sys/class/misc/ and /sys/devices/virtual/misc/ */
-	.mode = 0666,				 /* ... dev node perms set as specified here */
-	.fops = &fops,				 /* connect to this driver's 'functionality' */
+    .minor = MISC_DYNAMIC_MINOR,
+    .name = DEVICE_NAME,
+    .mode = 0666,
+    .fops = &fops,
 };
 
 static int __init buzzer_init(void) {
-    int result;
-    int err = 0;
-    struct device *device;
+    int err;
 
-    /* Request utilization of PWM0 device */
-	pwm_device = pwm_request(0, PWM_DEVICE_NAME);
-
-	if (IS_ERR(pwm_device))
-		return PTR_ERR(pwm_device);
-
+    pwm_device = pwm_request(0, PWM_DEVICE_NAME);
+    if (IS_ERR(pwm_device))
+        return PTR_ERR(pwm_device);
 
     err = misc_register(&buzzer_misc);
-	if (err) {
+    if (err) {
         pr_err("Failed to register misc device\n");
         pwm_free(pwm_device);
         return err;
     }
 
-
-	device = buzzer_misc.this_device;
-
-	dev_info(device, "Display7s driver registered succesfully. To talk to\n");
-	dev_info(device, "the driver try to cat and echo to /dev/%s.\n", DEVICE_NAME);
-	dev_info(device, "Remove the module when done.\n");
-
-    /* Initialize work structure (with function) */
-	INIT_WORK(&my_work, my_wq_function);
-
+    INIT_WORK(&my_work, play_next_note);
+    timer_setup(&melody_timer, timer_callback, 0);
     mutex_init(&buzzer_mutex);
 
     printk(KERN_INFO "buzzer: Module loaded\n");
@@ -198,8 +227,9 @@ static int __init buzzer_init(void) {
 }
 
 static void __exit buzzer_exit(void) {
-	flush_work(&my_work);
-	pwm_free(pwm_device);
+    del_timer_sync(&melody_timer);
+    flush_work(&my_work);
+    pwm_free(pwm_device);
     misc_deregister(&buzzer_misc);
     mutex_destroy(&buzzer_mutex);
     printk(KERN_INFO "buzzer: Module unloaded\n");
